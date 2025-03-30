1f07e8e0477fd5f9763571f7e99f6a7df69abff2
ingdia <d.ingabire2@alustudent.com>
2025-03-30 19:27:57 +0000

    creating class for managing the stock and addign main function

---
+++ Stock.py
@@ -0,0 +1,12 @@
+#!/usr/bin/bash python3
+class Shop:
+    def main(self):
+        """Display the menu and handle user choices."""
+        while True:
+            print("\nShop Management System")
+            print("1. Add item")
+            print("2. View items")
+            print("3. Delete an item")
+            print("4. Update an item")
+            print("5. Search for an item")
+            print("6. Exit")
